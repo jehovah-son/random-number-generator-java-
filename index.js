d5c9db4b2f283c3ed4c3528742b1d7e3fcf51dc4
jehovah-son <chukwukasuccess0@gmail.com>
2025-08-28 13:36:23 +0100

    `Added multiple labels and random number generation for each label`

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const MyButton = document.getElementById("myButton");
-const MyLabel = document.getElementById("myLabel");
+const MyLabel1 = document.getElementById("myLabel1");
+const MyLabel2 = document.getElementById("myLabel2");
+const MyLabel3 = document.getElementById("myLabel3");
 const min = 1;
 const max = 10;
 
-let RandomNumber;
+let RandomNumber1;
+let RandomNumber2;
+let RandomNumber3;
 
-MyButton.onclick = function (){
-    RandomNumber = Math.floor(Math.random() * max)+min;
-    MyLabel.textContent = RandomNumber;
-    console.log(RandomNumber);
-   
-}
+MyButton.onclick = function () {
+  RandomNumber1 = Math.floor(Math.random() * max) + min;
+  RandomNumber2 = Math.floor(Math.random() * max) + min;
+  RandomNumber3 = Math.floor(Math.random() * max) + min;
+  MyLabel1.textContent = RandomNumber1;
+  MyLabel2.textContent = RandomNumber2;
+  MyLabel3.textContent = RandomNumber3;
+  console.log(RandomNumber1, RandomNumber2, RandomNumber3);
+};
